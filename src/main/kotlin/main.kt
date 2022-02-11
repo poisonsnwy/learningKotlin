68a930a8ad6312abb9af681571420ec72cb24032
poisonsnwy <poisonsnwy@gmail.com>
2022-02-11 20:08:45 +0200

    We are testing branch system

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,13 @@
 fun main() {
-    val x = readLine()!!
-    val y = x.length / 2 - 1
-    var i = 0
-    while (i <= y) {
-        println(x[i])
-        i++
+    val x = readLine()
+    x?.let{
+        val y = x.length / 2 - 1
+
+        var i = 0
+        while (i <= y) {
+            print(x[i])
+            i++
+        }
     }
+
 }
